947aec7c9be7fd1e276d990a836aae64732c0e18
RahaelGeorge <rahaelg02@gmail.com>
2018-07-10 16:46:54 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,3 +2,15 @@
 
 # Try starting out with puts'ing a string.
 puts "What is your guest name?"
+x = gets.strip 
+puts "What is the Party name?"
+y = gets.strip
+puts "What is the date of your party?"
+z = gets.strip
+puts "What time will the party occur?"
+w = gets.strip
+puts "What is the host's name?"
+v = gets.strip
+
+puts "Dear #{x}, 
+You are cordially invited to the #{y} on #{z} at #{w}. Please RSVP no"
